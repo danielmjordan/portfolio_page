030af234bc749cd805c783bdb29b78a88e15980d
Daniel Jordan <danieljordan86@gmail.com>
2020-07-02 15:02:05 -0600

    CUse gatsby-image to progressively load profile photo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 module.exports = {
 	plugins: [
 		'@hashinteractive/gatsby-theme-reactor',
+		'gatsby-transformer-sharp',
+		'gatsby-plugin-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: `${__dirname}/src/data/images`,
+			},
+		},
 		{
 			resolve: 'gatsby-plugin-react-svg',
 			options: {
--- src/@hashinteractive/gatsby-theme-reactor/components/profile.js
+++ src/@hashinteractive/gatsby-theme-reactor/components/profile.js
@@ -1,54 +1,74 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
-import { useState } from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
+import { jsx } from 'theme-ui';
+import { useState } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 const Profile = () => {
-  const { allFile } = useStaticQuery(graphql`
-    query {
-      allFile(filter: { name: {glob: "profile*"}}) {
-        edges {
-          node {
-            publicURL
-            name
-          }
-        }
-      }
-    }
-  `)
-  const profile = allFile.edges.find(({ node: { name }}) => name === 'profile')
-  const profileAlt = allFile.edges.find(({ node: { name }}) => name === 'profile-alt')
+	const { allFile } = useStaticQuery(graphql`
+		query MyQuery {
+			allFile(filter: { name: { glob: "profile*" } }) {
+				edges {
+					node {
+						id
+						publicURL
+						name
+						childImageSharp {
+							fluid {
+								aspectRatio
+								base64
+								sizes
+								src
+								srcSet
+							}
+						}
+					}
+				}
+			}
+		}
+	`);
 
-  const [publicURL, setPublicURL] = useState(profile.node.publicURL)
+	const profile = allFile.edges.find(
+		({ node: { name } }) => name === 'profile'
+	);
+	const profileAlt = allFile.edges.find(
+		({ node: { name } }) => name === 'profile-alt'
+	);
 
-  return (
-    <div
-      sx={{
-        bg: theme => theme.colors.gray[3],
-        boxShadow: '2xl',
-        borderRadius: 'lg',
-        mb: '50px'
-      }}>
-      <img
-        sx={{
-          top: ['15px', '30px'],
-          right: ['-15px', '-35px'],
-          boxShadow: 'lg',
-          borderRadius: 'lg',
-          position: 'relative',
-          maxWidth: 'full'
-        }}
-        onMouseEnter={() => {
-          setPublicURL(profileAlt.node.publicURL)
-        }}
-        onMouseLeave={() => {
-          setPublicURL(profile.node.publicURL)
-        }}
-        src={publicURL}
-        alt="Profile" />
-    </div>
-  )
-}
+	const [imageSource, setImageSource] = useState(
+		profile.node.childImageSharp.fluid
+	);
 
-export default Profile
+	return (
+		<div
+			sx={{
+				bg: (theme) => theme.colors.gray[3],
+				boxShadow: '2xl',
+				borderRadius: 'lg',
+				mb: '50px',
+			}}
+		>
+			<Img
+				sx={{
+					top: ['15px', '30px'],
+					right: ['-15px', '-35px'],
+					boxShadow: 'lg',
+					borderRadius: 'lg',
+					position: 'relative',
+					maxWidth: 'full',
+				}}
+				onMouseEnter={() => {
+					setImageSource(profileAlt.node.childImageSharp.fluid);
+				}}
+				onMouseLeave={() => {
+					setImageSource(profile.node.childImageSharp.fluid);
+				}}
+				fluid={imageSource}
+				alt="Profile"
+			/>
+		</div>
+	);
+};
+
+export default Profile;
